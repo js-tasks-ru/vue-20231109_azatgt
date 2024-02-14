7c3b9e07ceb7eeb6db14d645d2f62a54f72729f9
azatgt1996 <azatgt1996@yandex.ru>
2024-02-14 10:11:42 +0300

    +project

--- 19-project/01-project/main.js
+++ 19-project/01-project/main.js
@@ -1,2 +1,2 @@
 // Укажите в качестве значения переменной ссылку на репозиторий с вашим проектом
-export const REPOSITORY_URL = '';
+export const REPOSITORY_URL = 'https://github.com/azatgt1996/meetups';
